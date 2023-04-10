cfe450e4d4b7dd171e9db946c9d22ecc433633d4
al <me@alasdairb.com>
2023-04-10 20:29:10 +0100

    use page data

---
+++ src/routes/+page.ts
@@ -0,0 +1,223 @@
+/** @type {import('./$types').PageLoad} */
+export function load({ params }) {
+	return {
+		tools: {
+			'Analytics Database (OLAP)': [
+				{
+					name: 'Tinybird',
+					logo: '',
+					url: 'https://www.tinybird.co/',
+					pros: [],
+					cons: [],
+					foss: false
+				},
+				{
+					name: 'RockSet',
+					logo: '',
+					url: 'https://rockset.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				}
+			],
+			'Transactional Database (OLTP)': [
+				{
+					name: 'PlanetScale',
+					logo: '',
+					url: 'https://planetscale.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				},
+				{
+					name: 'Tigris',
+					logo: '',
+					url: 'https://www.tigrisdata.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				}
+			],
+			'Search Index': [
+				{
+					name: 'Tigris',
+					logo: '',
+					url: 'https://www.tigrisdata.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				},
+				{
+					name: 'meilisearch',
+					logo: '',
+					url: 'https://www.meilisearch.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				}
+			],
+			'NoSQL/Document Database': [
+				{
+					name: 'Tigris',
+					logo: '',
+					url: 'https://www.tigrisdata.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				},
+				{
+					name: '',
+					logo: '',
+					url: '',
+					pros: [],
+					cons: [],
+					foss: false
+				}
+			],
+			'Web Analytics': [
+				{
+					name: 'Fathom',
+					logo: '',
+					url: 'https://usefathom.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				},
+				{
+					name: 'BeamAnalytics',
+					logo: '',
+					url: 'https://beamanalytics.io/',
+					pros: [],
+					cons: [],
+					foss: false
+				}
+			],
+			'Password Management': [
+				{
+					name: 'BitWarden',
+					logo: '',
+					url: 'https://bitwarden.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				},
+				{
+					name: '1Password',
+					logo: '',
+					url: 'https://1password.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				}
+			],
+			'Frontend hosting/deployment': [
+				{
+					name: 'Vercel',
+					logo: '',
+					url: 'https://vercel.com',
+					pros: [],
+					cons: [],
+					foss: false
+				},
+				{
+					name: 'Netlify',
+					logo: '',
+					url: 'https://www.netlify.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				}
+			],
+			'Cloud vendor': [
+				{
+					name: 'GCP',
+					logo: '',
+					url: 'https://cloud.google.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				},
+				{
+					name: 'AWS',
+					logo: '',
+					url: 'https://aws.amazon.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				}
+			],
+			'Frontend Framework (Apps)': [
+				{
+					name: 'NextJS',
+					logo: '',
+					url: 'https://nextjs.org/',
+					pros: [],
+					cons: [],
+					foss: false
+				},
+				{
+					name: 'SvelteKit',
+					logo: '',
+					url: 'https://kit.svelte.dev/',
+					pros: [],
+					cons: [],
+					foss: false
+				}
+			],
+			'Documentation Framework': [
+				{
+					name: 'Docusaurus',
+					logo: '',
+					url: 'https://docusaurus.io/',
+					pros: [],
+					cons: [],
+					foss: false
+				},
+				{
+					name: 'Nextra',
+					logo: '',
+					url: 'https://nextra.site/',
+					pros: [],
+					cons: [],
+					foss: false
+				}
+			],
+			'Documentation Language': [
+				{
+					name: 'Markdown',
+					logo: '',
+					url: 'https://www.markdownguide.org/',
+					pros: [],
+					cons: [],
+					foss: false
+				},
+				{
+					name: 'AsciiDoc',
+					logo: '',
+					url: 'https://asciidoc.org/',
+					pros: [],
+					cons: [],
+					foss: false
+				}
+			],
+			'Data Warehouse': [
+				{
+					name: 'BigQuery',
+					logo: '',
+					url: 'https://cloud.google.com/bigquery',
+					pros: [],
+					cons: [],
+					foss: false
+				},
+				{
+					name: 'Snowflake',
+					logo: '',
+					url: 'https://www.snowflake.com/',
+					pros: [],
+					cons: [],
+					foss: false
+				}
+			]
+		}
+	};
+}
